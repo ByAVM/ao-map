304c91981efe2438a7905c4667b0f7ad8690fa01
ByAMV <avdeevmiv@gmail.com>
2022-04-09 17:33:40 +0300

    Автоматизация деплоя на GH Pages

---
+++ scripts/deploy-gh-pages.js
@@ -0,0 +1,17 @@
+import("execa").then(async ({ execa }) => {
+  (async () => {
+    try {
+      console.log("Starting deployment..");
+      await execa("git", ["--work-tree", "dist", "add", "--all"]);
+      await execa("git", ["--work-tree", "dist", "commit", "-m", "gh-pages"]);
+      console.log("Pushing to gh-pages...");
+      await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+      await execa("git", ["checkout", "-f", "master"]);
+      await execa("git", ["branch", "-D", "gh-pages"]);
+      console.log("Successfully deployed, check your settings");
+    } catch (error) {
+      console.error(error.message);
+      process.exit(1);
+    }
+  })();
+});
