e15405e85e09ffa062d7e51c7157cc9856a26080
ByAMV <avdeevmiv@gmail.com>
2022-04-10 18:30:09 +0300

    chore: Деплой приложения с учетом выполнения хуков

--- scripts/deploy-gh-pages.js
+++ scripts/deploy-gh-pages.js
@@ -4,9 +4,9 @@ import("execa").then(async ({ execa }) => {
       await execa("git", ["checkout", "--orphan", "gh-pages"]);
       console.log("Starting deployment..");
       await execa("git", ["--work-tree", "dist", "add", "--all"]);
-      await execa("git", ["--work-tree", "dist", "commit", "-m", "gh-pages"]);
+      await execa("git", ["--work-tree", "dist", "commit", "-m", "gh-pages", "--no-verify"]);
       console.log("Pushing to gh-pages...");
-      await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+      await execa("git", ["push", "origin", "HEAD:gh-pages", "--force", "--no-verify"]);
       await execa("git", ["checkout", "-f", "master"]);
       await execa("git", ["branch", "-D", "gh-pages"]);
       console.log("Successfully deployed, check your settings");
